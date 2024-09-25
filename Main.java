3ba7ffcd7c33d77d44673b9959c2da83c7ef0689
Wesley-designs <jiewesleyd2789@nycstudents.net>
2024-09-25 13:24:12 +0000

    it finaly works

--- Main.java
+++ Main.java
@@ -3,16 +3,18 @@ public class Main
     public static void main(String[] args)
     {
         Feeder f = new Feeder(500);
-        Feeder g = new Feeder(500);
-        Feeder h = new Feeder(1400);
+        Feeder g = new Feeder(1000);
         Feeder i = new Feeder(100);
+        Feeder h = new Feeder(2400);
+        Feeder j = new Feeder(250);
+        Feeder k = new Feeder(0);
         System.out.println(f);
-        g.simulateOneDay(12);
+        g.simulateOneDay(22);
         System.out.println(g);
         i.simulateOneDay(5);
         System.out.println(i);
-        h.simulateOneDay(22);
-        System.out.println(h);
-        System.out.println(h.simulateManyDays(5, 10));
+        System.out.println(h.simulateManyDays(10, 4));
+        System.out.println(j.simulateManyDays(10, 5));
+        System.out.println(k.simulateManyDays(5, 10));
     }
 }
